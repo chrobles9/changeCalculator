1fbca79d465cd0e62b6b5c1445928fed4cb985fb
Carlos Robles <carloshrobles9@gmail.com>
2021-02-19 17:21:53 -0500

    JavaScript functionality created
    Basic JavaScript functionality added to get users input, calculate/display coin amounts and activate button click event listener.

--- js/calcScript.js
+++ js/calcScript.js
@@ -1,9 +1,44 @@
-// TODO: Get elements from doc 
+// Parse and calculate for each coin value
 
+function changeCalculate() {
+  // Gets users cents input
+  let cents = document.getElementById("centsInput").value;
 
-// TODO: Parse and calculate for each coin value 
+  // Stores new number to be calculated
+  let newNum = 0;
 
+  // Calculate quarters
+  newNum = parseInt(cents / 25);
+  // Displays quarter amount to label
+  document.getElementById("quarters").value = newNum;
 
+  // Calculate amount of change left after subtracting quarter amount
+  cents = cents - 25 * newNum;
 
+  // Calculate dimes
+  newNum = parseInt(cents / 10);
+  // display dime amount to label
+  document.getElementById("dimes").value = newNum;
 
-// TODO: Add button event listeners 
+  // Calculate amount of change left after subtracting dime amount
+  cents = cents - 10 * newNum;
+  // Calculate nickels
+  newNum = parseInt(cents / 5);
+  // display nickels amount to label
+  document.getElementById("nickels").value = newNum;
+
+  // Calculate amount of change left after subtracting nickel amount
+  cents = cents - 5 * newNum;
+  // Calculate pennies
+  newNum = parseInt(cents / 1);
+  // display nickel amount to label
+  document.getElementById("pennies").value = newNum;
+
+  // Calculate amount of change left over after subtracting penny amount
+  cents = cents - newNum;
+}
+
+// Get change element from doc and add button event listeners
+document
+  .getElementById("calculateBtn")
+  .addEventListener("click", changeCalculate, false);
