67fe2f8b31ce2ee90894953a6c9bf32325b7276d
Carlos Robles <carloshrobles9@gmail.com>
2021-02-19 17:30:18 -0500

    JavaScript functionality updated
    JavaScript functionality streamlined using loop and coin objects array.

--- js/calcScript.js
+++ js/calcScript.js
@@ -7,36 +7,24 @@ function changeCalculate() {
   // Stores new number to be calculated
   let newNum = 0;
 
-  // Calculate quarters
-  newNum = parseInt(cents / 25);
-  // Displays quarter amount to label
-  document.getElementById("quarters").value = newNum;
+  // Loop through coin array object to calculate and display proper change amounts
+  coins.forEach((coin) => {
+    // Calculates cent amount after dividing corresponding coin value
+    newNum = parseInt(cents / coin.value);
+    // Displays coin amount to corresponding label
+    document.getElementById(coin.name).value = newNum;
 
-  // Calculate amount of change left after subtracting quarter amount
-  cents = cents - 25 * newNum;
-
-  // Calculate dimes
-  newNum = parseInt(cents / 10);
-  // display dime amount to label
-  document.getElementById("dimes").value = newNum;
-
-  // Calculate amount of change left after subtracting dime amount
-  cents = cents - 10 * newNum;
-  // Calculate nickels
-  newNum = parseInt(cents / 5);
-  // display nickels amount to label
-  document.getElementById("nickels").value = newNum;
-
-  // Calculate amount of change left after subtracting nickel amount
-  cents = cents - 5 * newNum;
-  // Calculate pennies
-  newNum = parseInt(cents / 1);
-  // display nickel amount to label
-  document.getElementById("pennies").value = newNum;
-
-  // Calculate amount of change left over after subtracting penny amount
-  cents = cents - newNum;
+    cents = cents - coin.value * newNum;
+  });
 }
+
+// Array of coin objects
+const coins = [
+  { name: "quarters", value: 25 },
+  { name: "dimes", value: 10 },
+  { name: "nickels", value: 5 },
+  { name: "pennies", value: 1 },
+];
 
 // Get change element from doc and add button event listeners
 document
